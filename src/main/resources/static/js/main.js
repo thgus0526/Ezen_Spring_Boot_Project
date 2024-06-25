3778568244cb8821bd5b6d5d1bb7ad2a1588e2d6
jbn750 <jbn750@gmail.com>
2024-06-25 20:53:03 +0900

    처음 화면 나올 때 마커에 주소 출력

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -83,6 +83,9 @@ function mapXY(x, y) {
 // 현재 지도 중심좌표로 주소를 검색해서 지도 좌측 상단에 표시합니다
     searchAddrFromCoords(map.getCenter(), displayCenterInfo);
 
+    // 초기에 지도 중심 좌표에 대한 주소정보를 표시합니다
+    displayInitialInfo(markerPosition);
+
 // 지도를 클릭했을 때 클릭 위치 좌표에 대한 주소정보를 표시하도록 이벤트를 등록합니다
     kakao.maps.event.addListener(map, 'click', function(mouseEvent) {
         searchDetailAddrFromCoords(mouseEvent.latLng, function(result, status) {
@@ -125,6 +128,35 @@ function mapXY(x, y) {
     function searchDetailAddrFromCoords(coords, callback) {
         // 좌표로 법정동 상세 주소 정보를 요청합니다
         geocoder.coord2Address(coords.getLng(), coords.getLat(), callback);
+    }
+
+    // 초기에 마커 위치에 대한 주소정보를 표시하는 함수
+    function displayInitialInfo(position) {
+        searchDetailAddrFromCoords(position, function(result, status) {
+            if (status === kakao.maps.services.Status.OK) {
+                var detailAddr = !!result[0].road_address ? '<div>도로명주소 : ' + result[0].road_address.address_name + '</div>' : '';
+                detailAddr += '<div>지번 주소 : ' + result[0].address.address_name + '</div>';
+
+                var content = '<div class="bAddr">' +
+                    '<span class="title">법정동 주소정보</span>' +
+                    detailAddr +
+                    '</div>';
+
+                // 마커를 처음 위치에 표시합니다
+                marker.setPosition(position);
+
+                // 마커의 좌표를 infowindow에 표시합니다
+                var markerLat = position.getLat();
+                var markerLng = position.getLng();
+
+                // locationfunc 호출 예제
+                locationfunc(markerLng, markerLat);
+
+                // 인포윈도우에 처음 위치에 대한 법정동 상세 주소정보를 표시합니다
+                infowindow.setContent(content);
+                infowindow.open(map, marker);
+            }
+        });
     }
 
 // 지도 좌측상단에 지도 중심좌표에 대한 주소정보를 표출하는 함수입니다
